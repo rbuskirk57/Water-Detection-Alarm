e537e084791d765824bd09755210a7f2a6cd6167
rbuskirk57 <125936958+rbuskirk57@users.noreply.github.com>
2023-11-24 14:37:03 -0800

    Update Leak Detect.py
    Modified LED type and sensor 1 logic. Removed picozero and just used machine. Still need to add a 2nd sensor and MQTT logic.

--- Leak Detect.py
+++ Leak Detect.py
@@ -3,11 +3,12 @@ import secrets
 import net_connect
 import socket
 from time import sleep
-from picozero import pico_temp_sensor, pico_led, Button
-from machine import Pin
+#from picozero import pico_temp_sensor, pico_led, Button
+from machine import Pin, PWM
 import utime
 import machine
 from umqttsimple import MQTTClient
+import math
 
 def reset_pico():
    print('Bailing out with a machine soft reset...')
@@ -23,15 +24,24 @@ def open_socket(ip):
     connection.listen(1)
     return connection
 
-
+def pulse(l, t):
+    for i in range(10):
+        l.duty_u16(int(math.sin(i / 10 * math.pi) * 500 + 500))
+        utime.sleep_ms(t)
+        
 count=0
 count_h=0
 ip = '0'
 wifi_led = Pin(27, Pin.OUT) #WiFi connected
-alarm_led = Pin(13, Pin.OUT) #leak detected
-btn1 = Button(15)
+alarm_led = Pin(13, Pin.OUT) #leak detected RED or sensor not connected
+Sensor_1_OK_led = Pin(12, Pin.OUT) # Green if OK; Red if not
+Sensor_1 = Pin(15, Pin.IN, Pin.PULL_UP)
+Sensor_1_OK = Pin(14, Pin.IN, Pin.PULL_UP) # Sensor detected
+
+buzzer = PWM(Pin(19), freq=700)
 wifi_led.off()
 
+#pulse(buzzer, 10)
 sensor_temp = machine.ADC(4)
 conversion_factor = 3.3 / (65535)
 
@@ -39,23 +49,30 @@ ip = net_connect.connect(secrets.SSID, secrets.PASSWORD)
 if ip != "-1":
     print("connected on :" + ip)
     wifi_led.on()
-    #connection = open_socket(ip)
 else:
     reset_pico()
 
 while True:
-    if btn1.is_pressed:
-        print("button 1 is closed")
-        alarm_led.toggle()
+    #print("OK: " + str(Sensor_1_OK.value()))
+    if Sensor_1_OK.value():
+        #Leak detected
+        if not Sensor_1.value():
+            alarm_led.toggle()
+            Sensor_1_OK_led.on() # GREEN OFF
+            #pulse(buzzer, 25)
+            buzzer.duty_u16(32768) 
+        else: # No Leak Sensor Detected
+            Sensor_1_OK_led.off() # GREEN
+            alarm_led.on()
+            buzzer.duty_u16(0)
     else:
-        #print("button 1 is open")
-        alarm_led.off()
+        # No Sensor Detected
+        alarm_led.off() # RED ON
+        Sensor_1_OK_led.on() # GREEN OFF
+        buzzer.duty_u16(0) # turn off the buzzer
             
     reading = sensor_temp.read_u16() * conversion_factor 
     temperature_c = 27 - (reading - 0.706)/0.001721
     fahrenheit_degrees = temperature_c * (9 / 5) + 32
     Temp_F = "Pico Temperature: " + str(round(fahrenheit_degrees,2)) + " *F"
-    #print(Temp_F)
     utime.sleep(1)
-    
-
