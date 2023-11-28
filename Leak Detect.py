0b15508fb8311aac160d39fa9b6889d11f0d8a9c
rbuskirk57 <125936958+rbuskirk57@users.noreply.github.com>
2023-11-27 19:37:35 -0800

    Update Leak Detect.py
    Added MQTT code

--- Leak Detect.py
+++ Leak Detect.py
@@ -10,6 +10,17 @@ import machine
 from umqttsimple import MQTTClient
 import math
 
+def mqtt_serve(door):
+    client = mqtt_connect()
+    client.publish(mqtt_pub_params.topic_pub, msg=home)
+    utime.sleep(3)
+        
+def mqtt_connect():
+    client = MQTTClient(client_id, mqtt_server, user=user_t, password=password_t, keepalive=30)
+    client.connect()
+    print('Connected to %s MQTT Broker'%(mqtt_server))
+    return client
+
 def reset_pico():
    print('Bailing out with a machine soft reset...')
    time.sleep(1)
@@ -62,55 +73,70 @@ else:
     reset_pico()
 
 while True:
-    #print("OK: " + str(Sensor_1_OK.value()))
-    if Sensor_1_OK.value():
-        #Leak detected
-        #if not Sensor_1.value():
-        if btn1.is_pressed:
-            alarm_led_1.toggle()
+    try:
+        client = mqtt_connect()
+    except OSError as e:
+        reset_pico()
+
+    while True:
+        #print("OK: " + str(Sensor_1_OK.value()))
+        if Sensor_1_OK.value():
+            #Leak detected
+            #if not Sensor_1.value():
+            if btn1.is_pressed:
+                alarm_led_1.toggle()
+                Sensor_1_OK_led.on() # GREEN OFF
+                #buzzer.duty_u16(32768) # 50% duty cycle
+                buzzer.duty_ns(30000)
+                S1 = "ALARM_WATER_DETECTED_S1"
+            else: # No Leak Sensor Detected
+                Sensor_1_OK_led.off() # GREEN
+                alarm_led_1.on()
+                buzzer.duty_u16(0)
+                S1 = "NO_WATER_DETECTED_S1"
+        else:
+            # No Sensor Detected
+            alarm_led_1.off() # RED ON
             Sensor_1_OK_led.on() # GREEN OFF
-            #buzzer.duty_u16(32768) # 50% duty cycle
-            buzzer.duty_ns(30000)
-            S1 = "ALARM_WATER_DETECTED"
-        else: # No Leak Sensor Detected
-            Sensor_1_OK_led.off() # GREEN
-            alarm_led_1.on()
-            buzzer.duty_u16(0)
-            S1 = "NO_WATER_DETECTED"
-    else:
-        # No Sensor Detected
-        alarm_led_1.off() # RED ON
-        Sensor_1_OK_led.on() # GREEN OFF
-        buzzer.duty_u16(0) # turn off the buzzer
-        S1 = "NO_SENSOR"
+            buzzer.duty_u16(0) # turn off the buzzer
+            S1 = "NO_SENSOR_S1"
 
-    if Sensor_2_OK.value():
-        #Leak detected
-        #if not Sensor_2.value():
-        if btn2.is_pressed:
-            alarm_led_2.toggle()
+        if Sensor_2_OK.value():
+            #Leak detected
+            #if not Sensor_2.value():
+            if btn2.is_pressed:
+                alarm_led_2.toggle()
+                Sensor_2_OK_led.on() # GREEN OFF
+                #buzzer.duty_u16(32768)
+                buzzer.duty_ns(30000)
+                #print("alarm 2: " + str(Sensor_2.value()))
+                S2 = "ALARM_WATER_DETECTED_S2"
+            else: # No Leak Sensor Detected
+                Sensor_2_OK_led.off() # GREEN
+                alarm_led_2.on()
+                buzzer.duty_u16(0)
+                S2 = "NO_WATER_DETECTED_S2"
+        else:
+            # No Sensor Detected
+            alarm_led_2.off() # RED ON
             Sensor_2_OK_led.on() # GREEN OFF
-            #buzzer.duty_u16(32768)
-            buzzer.duty_ns(30000)
-            #print("alarm 2: " + str(Sensor_2.value()))
-            S2 = "ALARM_WATER_DETECTED"
-        else: # No Leak Sensor Detected
-            Sensor_2_OK_led.off() # GREEN
-            alarm_led_2.on()
-            buzzer.duty_u16(0)
-            S2 = "NO_WATER_DETECTED"
-    else:
-        # No Sensor Detected
-        alarm_led_2.off() # RED ON
-        Sensor_2_OK_led.on() # GREEN OFF
-        S2 = "NO_SENSOR"
-            
+            S2 = "NO_SENSOR_S2"
+                
 
-    reading = sensor_temp.read_u16() * conversion_factor 
-    temperature_c = 27 - (reading - 0.706)/0.001721
-    fahrenheit_degrees = temperature_c * (9 / 5) + 32
-    Temp_F = "Pico Temperature: " + str(round(fahrenheit_degrees,2)) + " *F"
-    utime.sleep(.5)
-    
-
-
+        reading = sensor_temp.read_u16() * conversion_factor 
+        temperature_c = 27 - (reading - 0.706)/0.001721
+        fahrenheit_degrees = temperature_c * (9 / 5) + 32
+        Temp_F = "Pico Temperature: " + str(round(fahrenheit_degrees,2)) + " *F"
+        #utime.sleep(.5)
+        
+        try:
+            client.publish(mqtt_pub_params.topic_pub, msg=S1)
+            client.publish(mqtt_pub_params.topic_pub, msg=S2)
+            client.publish(mqtt_pub_params.topic_pub, msg=Temp_F)
+            utime.sleep(1)
+        except:
+            print("Client publish failed, executing a machine reset")
+            reset_pico()
+            pass
+    print("client disconnect")
+    client.disconnect()
